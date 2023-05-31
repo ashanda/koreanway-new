feac0f4f0e1fe438075eecfe22225f78efd41b51
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2023-05-31 14:50:44 +0530

    class styles

--- resources/views/pages/student/smart-class.blade.php
+++ resources/views/pages/student/smart-class.blade.php
@@ -5,59 +5,59 @@
 <div class="row">
     @foreach ($lessonsPaginated as $lesson)
     <div class="col-sm-12 col-md-6 col-lg-4">
-        <div class="card">
-            <img class="card-img-top" src="{{ asset('/lesson/img/' . $lesson->image) }}" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">{{ $lesson->title }}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                @if (StudentPaymentCheck($lesson->course_id,$lesson->batch_id ) == null)
-                <a href="#" onclick="openModel({{ $lesson->course_id }}, {{ $lesson->batch_id }},{{ $lesson->teacher_id }})" class="save_btn btn-block payment-here">No Acess | Payment Here</a>
-                @else
-                @if (StudentPaymentCheck($lesson->course_id,$lesson->batch_id)->status == 1 && StudentPaymentCheck($lesson->course_id,$lesson->batch_id)->end_date >= now()->toDateString() && StudentPaymentCheck($lesson->course_id,$lesson->batch_id)->end_date >= $lesson->published_date)
-                @php
+        <div class="cl_vid">
+            <div class="card p-0">
+                <div class="vid_sec">
+                    <!-- if image -->
+                    <div class="img" style="background-image: url('{{ asset('/lesson/img/' . $lesson->image) }}');">
+                    </div>
+                    <!-- if image -->
+                    <!-- if video -->
 
-                $encryptedLessonid = encrypt(['id' => $lesson->id]);
-                @endphp
-                <a href="{{ route('smartClassData',$lesson->id ) }}" class="btn btn-primary">GET LESSEONS CLICK HERE</a>
-                <p> VALID PERIODE {{ StudentPaymentCheck($lesson->course_id,$lesson->batch_id)->start_date .'-'. StudentPaymentCheck($lesson->course_id,$lesson->batch_id)->end_date }}</p>
-                {{-- <a href="{{ route('single-lesson', ['lessontype' => $encryptedLessontype, 'id' => $encryptedLessonid]) }}" class="save_btn btn-block">Watch Lesson</a> --}}
-                @elseif (StudentPaymentCheck($lesson->course_id,$lesson->batch_id)->status == 2)
-                <a href="student_profile.php" class="save_btn btn-block">Your Payment is Pending</a>
-                @else
-                <a href="#" onclick="openModel({{ $lesson->course_id }}, {{ $lesson->batch_id }},{{ $lesson->teacher_id }})" class="save_btn btn-block payment-here">Payment Here</a>
-                @endif
-                @endif
+                    <!-- <iframe class="" id="ytplayer" type="text/html" src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=0&origin=http://example.com" frameborder="0">
+                    </iframe> -->
 
+                    <!-- if video -->
+                    <!-- if not payed  -->
+                    <div class="bg-lock">
+                        <i class="bi bi-lock"></i>
+                    </div>
+                    <!-- if not payed  -->
+                </div>
+
+                <div class="card-body p-0">
+                    <div class="alert alert-light text-center mb-0" role="alert">
+                        {{ $lesson->title }}
+                    </div>
+                    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                    @if (StudentPaymentCheck($lesson->course_id,$lesson->batch_id ) == null)
+                    <a href="#" onclick="openModel({{ $lesson->course_id }}, {{ $lesson->batch_id }},{{ $lesson->teacher_id }})" class="btn btn-danger w-100">No Acess | Payment Here</a>
+                    @else
+                    @if (StudentPaymentCheck($lesson->course_id,$lesson->batch_id)->status == 1 && StudentPaymentCheck($lesson->course_id,$lesson->batch_id)->end_date >= now()->toDateString() && StudentPaymentCheck($lesson->course_id,$lesson->batch_id)->end_date >= $lesson->published_date)
+                    @php
+
+                    $encryptedLessonid = encrypt(['id' => $lesson->id]);
+                    @endphp
+                    <a href="{{ route('smartClassData',$lesson->id ) }}" class="btn btn-secondary w-100">Get lessons click here</a>
+                    <div class="alert alert-primary text-center mb-0" role="alert">
+                        Valid period {{ StudentPaymentCheck($lesson->course_id,$lesson->batch_id)->start_date .'-'. StudentPaymentCheck($lesson->course_id,$lesson->batch_id)->end_date }}
+                    </div>
+                    {{-- <a href="{{ route('single-lesson', ['lessontype' => $encryptedLessontype, 'id' => $encryptedLessonid]) }}" class="save_btn btn-block">Watch Lesson</a> --}}
+                    @elseif (StudentPaymentCheck($lesson->course_id,$lesson->batch_id)->status == 2)
+                    <div class="alert alert-warning text-center mb-0" role="alert">
+                        Your Payment is Pending
+                    </div>
+                    @else
+                    <a href="#" onclick="openModel({{ $lesson->course_id }}, {{ $lesson->batch_id }},{{ $lesson->teacher_id }})" class="btn btn-success w-100">Payment Here</a>
+                    @endif
+                    @endif
+
+                </div>
             </div>
         </div>
     </div>
     @endforeach
-    <div class="col-sm-12 col-md-6 col-lg-4">
-        <div class="cl_vid">
-            <div class="card p-0">
-                <!-- <img class="card-img-top" src="{{ asset('/lesson/img/' . $lesson->image) }}" alt="Card image cap"> -->
-                <iframe class="" id="ytplayer" type="text/html" src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com" frameborder="0"></iframe>
-                <div class="card-body p-0">
-                    <a href="#" onclick="" class="btn btn-primary w-100">No Acess | Payment Here</a>
-                    <a href="{{ route('smartClassData',$lesson->id ) }}" class="btn btn-secondary w-100">GET LESSEONS CLICK HERE</a>
-                    <div class="alert alert-light text-center mb-0" role="alert">
-                        Title
-                    </div>
-                    <div class="alert alert-primary text-center mb-0" role="alert">
-                        VALID PERIODE
-                    </div>
-                    <div class="alert alert-warning text-center mb-0" role="alert">
-                        Your Payment is Pending
-                    </div>
-
-                    <a href="#" onclick="" class="btn btn-success w-100">Payment Here</a>
-                    {{-- <a href="{{ route('single-lesson', ['lessontype' => $encryptedLessontype, 'id' => $encryptedLessonid]) }}" class="save_btn btn-block">Watch Lesson</a> --}}
-                </div>
-            </div>
-        </div>
-
-    </div>
-
+    
 </div>
 
 
